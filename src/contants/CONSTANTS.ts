d3fd4ecee504d25222bbbdf2d60e6e669a0d865a
Pavel Kulyasov <pavel.kulyasov@gmail.com>
2023-10-23 06:02:06 +0300

    add: zod rhf resolvers and skills page

--- src/contants/CONSTANTS.ts
+++ src/contants/CONSTANTS.ts
@@ -6,6 +6,6 @@ export const CONSTANTS = {
 	GITHUB_LINK: 'https://github.com/',
 	LINKEDIN_LINK: 'https://linkedin.com/',
 	ABOUT_ME_DESC: 'An accomplished System Administrator and DevOps enthusiast, I\'m dedicated to optimizing IT landscapes and delivering outstanding system performance. With a strong foundation in IT operations and development, I excel in collaborating with cross-functional teams, ensuring seamless communication, and delivering innovative solutions. Let\'s transform technology together.',
-	SKILLS_DESC: 'I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.',
+	SKILLS_DESC: 'Harnessing cutting-edge technologies to streamline development, deployment, and operations for top-notch efficiency and reliability.',
 	SCROLL_BTN: 'Scroll',
 }
---
+++ src/data/skillsData.ts
@@ -0,0 +1,39 @@
+interface ISkill {
+	title: string
+	value: number
+}
+
+export const skillsData: ISkill[] = [
+	{
+		title: 'Linux',
+		value: 90,
+	},
+	{
+		title: 'Ansible',
+		value: 85,
+	},
+	{
+		title: 'SQL/NoSQL',
+		value: 85,
+	},
+	{
+		title: 'SQL/NoSQL',
+		value: 85,
+	},
+	{
+		title: 'SQL/NoSQL',
+		value: 85,
+	},
+	{
+		title: 'SQL/NoSQL',
+		value: 85,
+	},
+	{
+		title: 'SQL/NoSQL',
+		value: 85,
+	},
+	{
+		title: 'SQL/NoSQL',
+		value: 85,
+	},
+]
